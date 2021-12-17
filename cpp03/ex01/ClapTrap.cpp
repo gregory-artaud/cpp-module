364d40af122f8c9ce67b04c9a1d7f76755562cf9
Gregory Artaud <gregory.rtd@gmail.com>
2021-12-17 08:20:39 +0100

    ready for push

--- cpp03/ex01/ClapTrap.cpp
+++ cpp03/ex01/ClapTrap.cpp
@@ -53,21 +53,21 @@ ClapTrap &ClapTrap::operator=(ClapTrap const &rhs)
 void ClapTrap::attack(std::string const &target)
 {
 	if (target.empty())
-		std::cout << "FR4G-TP " << this->name << " attacks nothing!" << std::endl;
+		std::cout << this->name << " attacks nothing!" << std::endl;
 	else
-		std::cout << "FR4G-TP " << this->name << " attacks " << target
+		std::cout << this->name << " attacks " << target
 			<< ", causing " << this->hit_points << " points of damage!" << std::endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-	std::cout << "FR4G-TP " << this->name << " took " << amount <<
+	std::cout << this->name << " took " << amount <<
 		" points of damage!" << std::endl;
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	std::cout << "FR4G-TP " << this->name << " got repaired " << amount <<
+	std::cout << this->name << " got repaired " << amount <<
 		" points!" << std::endl;
 }
 
--- cpp03/ex03/ClapTrap.cpp
+++ cpp03/ex03/ClapTrap.cpp
@@ -58,21 +58,21 @@ ClapTrap &ClapTrap::operator=(ClapTrap const &rhs)
 void ClapTrap::attack(std::string const &target)
 {
 	if (target.empty())
-		std::cout << "CL4P-TP " << this->name << " attacks nothing!" << std::endl;
+		std::cout << this->name << " attacks nothing!" << std::endl;
 	else
-		std::cout << "CL4P-TP " << this->name << " attacks " << target
+		std::cout << this->name << " attacks " << target
 			<< ", causing " << this->hit_points << " points of damage!" << std::endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-	std::cout << "CL4P-TP " << this->name << " took " << amount <<
+	std::cout << this->name << " took " << amount <<
 		" points of damage!" << std::endl;
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	std::cout << "CL4P-TP " << this->name << " got repaired " << amount <<
+	std::cout << this->name << " got repaired " << amount <<
 		" points!" << std::endl;
 }
 
