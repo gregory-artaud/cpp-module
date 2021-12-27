608e25c9a388ae3bb7737f67004d84c118729033
Grégory Artaud <gregory.rtd@gmail.com>
2021-12-27 22:08:23 +0100

    wip

---
+++ cpp06/ex00/Converter.cpp
@@ -0,0 +1,107 @@
+#include "Converter.hpp"  
+	
+Converter::Converter()
+{
+	this->rawData = "";
+	this->dataType = UNKNOWN;
+}
+
+Converter::Converter(Converter & src): rawData(src.rawData), dataType(src.dataType)
+{
+
+}
+
+Converter::Converter(std::string rawData)
+{
+	this->rawData = rawData;
+	this->dataType = getDataType(rawData);
+}
+
+Converter::~Converter()
+{
+	
+}
+
+Converter & Converter::operator=(const Converter & rhs)
+{
+	this->rawData = rhs.rawData;
+	this->dataType = rhs.dataType;
+
+	return *this;
+}
+
+const int & Converter::getDataType() const
+{
+	return this->dataType;
+}
+
+const std::string & Converter::getRawData() const
+{
+	return this->rawData;
+}
+
+std::string Converter::toChar() const
+{
+	return "";
+}
+
+std::string Converter::toInt() const
+{
+	return "";
+}
+
+std::string Converter::toFloat() const
+{
+	return "";
+}
+
+std::string Converter::toDouble() const
+{
+	return "";
+}
+
+int Converter::getDataType(std::string & data) const
+{
+	size_t i;
+	int n_dot;
+
+	n_dot = 0;
+	if (data.empty())
+		return UNKNOWN;
+	if (data == "-inf" || data == "-inff")
+		return MINFINITY;
+	if (data == "inf" || data == "inff")
+		return INFINITY;
+	if (!isdigit(data.at(0)) && data.size() == 1)
+		return CHAR;
+	if (data.at(0) == '-' || isdigit(data.at(0)))
+	{
+		i = 1;
+		while (isdigit(data[i]) && i < data.size())
+			i++;
+		if (i == data.size())
+			return INT;
+		i = 1;
+		while (n_dot <= 1 && i < data.size())
+		{
+			if (data[i] == '.')
+				n_dot++;
+			if (!isdigit(data[i]) && data[i] != '.' && (data[i] != 'f' || i != data.size() - 1))
+				return NAN;
+			i++;
+		}
+	}
+	else
+		return NAN;
+	if (n_dot > 1)
+		return NAN;
+	return (data.at(data.size() - 1) == 'f') ? FLOAT : DOUBLE;
+}
+
+std::ostream &operator<<(std::ostream &out, Converter const &c)
+{
+	out << "rawData = " << c.getRawData() << std::endl;
+	out << "dataType = " << c.getDataType() << std::endl;
+
+	return out;
+}
---
+++ cpp06/ex00/Converter.hpp
@@ -0,0 +1,47 @@
+#ifndef CONVERTER_H
+#define CONVERTER_H
+#pragma once
+
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+enum dataType {
+	UNKNOWN,
+	CHAR,
+	INT,
+	FLOAT,
+	DOUBLE,
+	NAN,
+	INFINITY,
+	MINFINITY
+};
+
+class Converter  
+{
+	private:
+		std::string rawData;
+		int dataType;
+		
+		int getDataType(std::string & data) const;
+
+	public:
+
+		Converter();
+		Converter(Converter & src);
+		Converter(std::string rawData);
+		~Converter();
+
+		Converter & operator=(const Converter &rhs);
+		const int & getDataType() const;
+		const std::string & getRawData() const;
+		std::string toChar() const;
+		std::string toInt() const;
+		std::string toFloat() const;
+		std::string toDouble() const;
+
+};
+
+std::ostream &operator<<(std::ostream &out, Converter const &c);
+
+#endif
